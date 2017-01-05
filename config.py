d586b8d4c008e18803890b6c21d1b11e10130aac
Alex Pan <alpan@Alexs-MacBook-Pro.local>
2017-01-04 21:33:30 -0500

    updated database initialization

--- config.py
+++ config.py
@@ -4,6 +4,10 @@ SECRET_KEY = 'password'
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
