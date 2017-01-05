ffb025d9077f7d42ef2db989c6742768b30ad46e
Alex Pan <alpan@Alexs-MacBook-Pro.local>
2017-01-04 22:35:23 -0500

    fixed syntax error in views.py

--- app/views.py
+++ app/views.py
@@ -97,7 +97,7 @@ def login():
 			remember_me = session['remember_me']
 			session.pop('remember_me', None)
 		login_user(user, remember = remember_me)
-		return url_for('index'))
+		return url_for('index')
 	return render_template('login.html',
 							form=form)
 
